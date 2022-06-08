41f1402be9b7579a299bc4a740d885ba65f81881
Tatsana Srisawang <tatsanasrisawang2@gmail.com>
2022-06-08 18:32:46 +0700

    chat

--- js/chat.js
+++ js/chat.js
@@ -35,7 +35,7 @@ function getTime() {
 
 // Gets the first message
 function firstBotMessage() {
-    let firstMessage = "วันนี้สบายดีไหมคะ"
+    let firstMessage = "สบายดีไหมคะ"
     document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
     let time = getTime();
